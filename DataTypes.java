3644d4c0df145d4f6b1722cc910de155b89f28ec
Sabrina <sabrina.conklin@vtsd.com>
2022-09-20 10:41:09 -0400

    Lab03 complete

--- DataTypes.java
+++ DataTypes.java
@@ -6,11 +6,19 @@
 
  */
 public class DataTypes {
+    public static void main(String args[]){
+        int sabrinaFavInt = 5;
+        double snicker = 7.5;
+        boolean likedogs = true;
 
-    // Variable syntax: dataType varName = value;
-    int uriahFavInt = 5;
-    double snicker = 7.5;
-    boolean likedogs = true;
+        System.out.println("Sabrina's Favorite Integer:" + sabrinaFavInt + " and the class snicker was " + snicker);
+
+        int n1 = 3;
+        int n2 = 4;
+        System.out.print(n1 + n2 / n1);
+    }
+
+
 
 }
 
---
+++ Lab03.java
@@ -0,0 +1,35 @@
+public class Lab03 {
+
+
+    public static void main(String args[]) {
+
+
+        int num = 10000123;
+        System.out.println();
+        System.out.println("Lab03, 100 point version/n");
+        System.out.println();
+        System.out.println("Starting milliseconds: 10000123");
+        System.out.print("Hours:");
+        System.out.print("                 ");
+        System.out.print(num/3600000);
+        System.out.println();
+        System.out.print("Minutes:");
+        System.out.print("               ");
+        System.out.print(num/215000);
+        System.out.println();
+        System.out.print("Seconds:");
+        System.out.print("               ");
+        System.out.print(num/245000);
+        System.out.println();
+        System.out.print("Milliseconds:");
+        System.out.print("          ");
+        System.out.print(num/81010);
+        System.out.println();
+
+
+
+    }
+
+}
+
+
