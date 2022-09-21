d008b9352471b37ffbd75bd257654b64647fcbc4
Sabrina <sabrina.conklin@vtsd.com>
2022-09-21 10:44:23 -0400

    Lab03 complete

--- Lab03.java
+++ Lab03.java
@@ -10,15 +10,15 @@ public class Lab03 {
         System.out.println("Lab03, 100 point version/n");
         System.out.println();
         int milli = 10000123;
-        System.out.println("Starting milliseconds: "   + milli);
+        System.out.println("Starting milliseconds:  "  + milli);
         int Hours = startingvalue/3600000;
-        System.out.println("Hours: \t\t\t" + Hours);
+        System.out.println("Hours: \t\t\t\t\t" + Hours);
         int Minutes = 2800/60;
-        System.out.println("Minutes: \t\t" + Minutes);
+        System.out.println("Minutes: \t\t\t\t" + Minutes);
         int Seconds = 2800 % 60;
-        System.out.println("Seconds: \t\t" + Seconds);
+        System.out.println("Seconds: \t\t\t\t" + Seconds);
         int Milliseconds = startingvalue % 1000;
-        System.out.println("Milliseconds \t" + Milliseconds);
+        System.out.println("Milliseconds: \t\t\t" + Milliseconds);
 
 
 
